e35454081af5f4bda2a3e4b14ae198c6b6e71715
PrestonCazier <32598096+PrestonCazier@users.noreply.github.com>
2017-11-11 22:56:59 -0800

    Create main.c
    Initial creation of the main file if we do a C implementation of the L1 cache simulation

---
+++ Code_C/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void fReadLine(char* line, FILE* fp);
+
+int main(int argc, char* argv[])
+{
+  FILE* fp;
+  char* mode;
+  
+  if (argc == 0)
+  {
+  }
+  
+  fp = fopen(argv[], mode);
+  return 0;
+}
+
+void fReadLine(char* line, FILE* fp)
+{
+}
