366252bdbd2f789b04c2a20713333494dae3473e
PrestonCazier <32598096+PrestonCazier@users.noreply.github.com>
2017-11-15 12:29:28 -0800

    Update main.cpp

--- Code_CPlusPlus/main.cpp
+++ Code_CPlusPlus/main.cpp
@@ -1,12 +1,15 @@
-#include <iostream>
-#include <string>
-#include <fstream>
 #include <iomanip>
+#include <stdio.h>
+#include <conio.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <bitset>
+#include <string>
 #include "cache.h"
 
 using namespace std;
 
-void openFile(ifstream &inFile, char* fileName);
 void ResetCache();
 
 int HitCount = 0;
@@ -17,5 +20,33 @@ long totalOperations = 0;
 
 int main(int argc, char* argv[])
 {
+  string line;
+  ifstream* file;
+  
+  try
+  {
+    file.open(argv[1]);
+    if (file.isOpen())
+    {
+      while (getline(file, line))
+      {
+        istringstream iss(line);
+        int a, b;
+        // process pair (a,b)
+        if (!(iss >> a >> b)) { break; } // error
+      
+      
+        // this is were we actually handle cache instructions
+        cout << a << ',' << b << '\n';
+        //
+      }
+    }
+  }
+  catch(Exception e)
+  {
+    cout << e << "\n\n";
+  }
+  
+  file.close();
   return 0;
 }
