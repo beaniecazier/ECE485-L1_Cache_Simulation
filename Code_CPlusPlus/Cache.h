9766f5060c25fd93d89b0291c22c9a8a9a8b194e
Armaan Roshani <armaan@gmail.com>
2017-11-18 15:30:52 -0800

    adding functions

--- Code_CPlusPlus/Cache.h
+++ Code_CPlusPlus/Cache.h
@@ -11,10 +11,20 @@ public:
 	~Cache(); //destructor
 	
 	//put primitives here (function declarations)
+	int readData(int address);
+	int writeData(int address);
+	int invalidataData(int address);
+	int readDataToL2(int address);
+		//
+	
 
 private:
 	// 16k sets
 	Set sets[NUM_SETS];
 	
 	//put variables here
+	bool verbose;
+	
+	int getIndex(int address);
+	int getTag(int address); //pass this to Dustin
 };
