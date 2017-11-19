8cdb73513d5c72635bd3f28a28293d7b7045fb1e
Armaan Roshani <armaan@gmail.com>
2017-11-18 16:57:36 -0800

    Created getTag and getIndex

--- Code_CPlusPlus/Cache.cpp
+++ Code_CPlusPlus/Cache.cpp
@@ -1,16 +1,38 @@
 #include "Cache.h"
 
 
-Cache::Cache(int associativity)
+Cache::Cache(int associativity, bool verbose)
 {
-	for each (Set set in sets)
+	for each (Set set in sets) //classes are defined with uppercase; sets is a variable
 	{
-		set = new Set(associativity);
+		set = new Set(associativity, verbose);
 	}
-	
-	//put function definitions here
 }
 
 Cache::~Cache()
 {
+	//delete everything!! Maybe?
 }
+
+
+//Private function definitions here
+int getIndex(int address)
+{
+	address = address << TAG_BITS; //bitshift to left to erase tag bits
+	return address = addres >> (TAG_BITS + OFFSET_BITS); //bitshift to right to erase offset bits and 
+							     //shift index bits back into position.
+}
+
+int getTag(int address)
+{
+	//bitshift to the right 20 bits (32 proc bits - 12 tag bits)
+	return address = address >> ((PROC_SIZE - TAG_BITS); 
+}
+
+
+//Public function definitions here
+Cache::readData(int address)
+{
+	index = getIndex(int address)
+		
+	
