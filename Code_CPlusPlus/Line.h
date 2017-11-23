7d95f2f3ba9525c3d5d99d04bf1b55d8df0e4204
PrestonCazier <32598096+PrestonCazier@users.noreply.github.com>
2017-11-23 12:17:10 -0800

    Create Line.h

---
+++ Code_CPlusPlus/Line.h
@@ -0,0 +1,22 @@
+#ifndef LINE_H_20171123
+#define LINE_H_20171123
+
+enum MESI
+{
+	MODIFIED = 0,
+	INVALID = 1,
+	SHARED = 2,
+	EXCLUSIVE = 3,
+};
+
+class Line 
+{
+public:
+	Line();
+	~Line();
+  
+private:
+	int tag;
+	int LRU;
+	MESI mesi;
+}
