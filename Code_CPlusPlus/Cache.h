d9f91249ab0d0f66a5b3a717a1e62ca7a674df8a
PrestonCazier <32598096+PrestonCazier@users.noreply.github.com>
2017-11-23 11:52:49 -0800

    Update Cache.h

--- Code_CPlusPlus/Cache.h
+++ Code_CPlusPlus/Cache.h
@@ -8,14 +8,13 @@
 #define CACHE_H_20171123
 
 #define PROC_SIZE = 32 //because Processor is 32-bit 
-#define NUM_SETS 16384 //because 16K sets = 2^14
 #define OFFSET_BITS 6 //because 64B lines = (2^6)B
 #define TAG_BITS 12 //because sets + lines = 14+6=20, 32-20=12
 
 class Cache
 {
 public:
-	Cache(int associativity, bool verbose);  //constructor
+	Cache(int setSize, int associativity, bool verbose);  //constructor
 	~Cache(); //destructor
 	
 	//public function declarations (primitives, prototypes)
