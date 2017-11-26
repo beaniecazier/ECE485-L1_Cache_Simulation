c6c4e37d6ef070346a592c0aec7771748475cef9
PrestonCazier <32598096+PrestonCazier@users.noreply.github.com>
2017-11-26 12:07:34 -0800

    Add files via upload

---
+++ Code_CPlusPlus/src/Cache.cpp
@@ -0,0 +1,112 @@
+#include "Cache.h"
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+Cache::Cache(int size, int a, bool v)
+{
+	setSize = size;
+	associativity = a;
+	verbose = v;
+	//sets = new Set(a, v)[setSize];
+	HitCount = 0;
+	MissCount = 0;
+	CacheReads = 0;
+	CacheWrites = 0;
+	totalOperations = 0;
+	//for (int i = 0; i < setSize; i++) //classes are defined with uppercase; sets is a variable
+	//{
+		//sets[i] = new Set(associativity, verbose);
+	//}
+}
+
+Cache::~Cache()
+{
+	// delete dynamic variables
+	//for (int i = 0; i < setSize; i++)
+	//{
+		//delete set[i];
+	//}
+	delete [] sets;
+}
+
+//Private function definitions here
+int Cache::getIndex(int address)
+{
+	address = address << TAG_BITS; //bitshift to left to erase tag bits
+	return address = address >> (TAG_BITS + OFFSET_BITS); //bitshift to right to erase offset bits and 
+							     //shift index bits back into position.
+}
+
+int Cache::getTag(int address)
+{
+	//bitshift to the right 20 bits (32 proc bits - 12 tag bits)
+	int shiftAmount = (PROC_SIZE - TAG_BITS);
+	return address = address >> shiftAmount; 
+}
+
+//Public function definitions here
+void Cache::readData(int address)
+{
+	totalOperations++;
+	CacheReads++;
+	int hit = sets[getIndex(address)].read(getTag(address), address);
+	HitCount += (hit) ? 1: 0;
+	MissCount += (!hit) ? 1 : 0;
+}
+
+void Cache::writeData(int address)
+{
+	int hit = sets[ getIndex( address) ].write( getTag( address), address);
+	totalOperations++;
+	CacheWrites++;
+	HitCount += (hit) ? 1 : 0;
+	MissCount += (!hit) ? 1 : 0;
+}
+
+void Cache::invalidate(int address)
+{
+	sets[ getIndex( address) ].invalidate( getTag( address) );
+}
+
+void Cache::readFromL2(int address)
+{
+	int hit = sets[ getIndex( address) ].readFromL2( getTag( address), address);
+	totalOperations++;
+	CacheWrites++;
+	HitCount += (hit) ? 1 : 0;
+	MissCount += (!hit) ? 1 : 0;
+}
+
+void Cache::resetAll()
+{
+	for (int i = setSize; i > 0; --i)
+	{
+		sets[i].reset();
+	}
+	totalOperations = 0;
+	CacheReads = 0;
+	CacheWrites = 0;
+	HitCount = 0;
+	MissCount = 0;
+}
+
+void Cache::printCache()
+{
+	cout << setw(7) << "INDEX" << "\t" << setfill(' ') << setw(3) << "LRU" << setw(7) << "TAG" << setw(10) << "MESI";
+	for( int i = 0; i < setSize; i++)
+	{
+		sets[ i].print(i);
+	}
+}
+
+void Cache::printStatistics()
+{
+	double hitRate = ((double)HitCount) / ((double)totalOperations) * 100.0;
+	double missRate = ((double)MissCount) / ((double)totalOperations) * 100.0;
+	
+	cout << "out of " << totalOperations << " total operations\n"
+		 << "There were " << CacheReads << " cache reads and " << CacheWrites << " cache writes\n"
+		 << "The hit and miss rate was " << setprecision(2) << hitRate << " and " << missRate << "\n";
+}
