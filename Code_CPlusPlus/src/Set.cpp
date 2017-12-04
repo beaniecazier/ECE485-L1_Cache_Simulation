63da281f53309c831ba69536fed6fb4f3b8cb88d
PrestonCazier <32598096+PrestonCazier@users.noreply.github.com>
2017-12-03 19:49:19 -0800

    Add files via upload

--- Code_CPlusPlus/src/Set.cpp
+++ Code_CPlusPlus/src/Set.cpp
@@ -87,15 +87,8 @@ int Set::write(unsigned int tag)
 		{
 			if (lines[i].tag == tag)		// find a hit
 			{
-				if (lines[i].mesi == EXCLUSIVE) 
-				{
-					lines[i].mesi = MODIFIED;
-				}
-				else if (lines[i].mesi != MODIFIED)
-				{
-					lines[i].mesi = EXCLUSIVE;
-					cout << "Write to L2 " << htos(reconstructAddress(tag)) << endl;
-				}
+				if (lines[i].mesi == EXCLUSIVE) lines[i].mesi = MODIFIED;
+				else if (lines[i].mesi != MODIFIED) lines[i].mesi = EXCLUSIVE;
 				touch(tag);					// this handles LRU for this hit
 				return HIT;
 			}
@@ -355,6 +348,10 @@ void Set::setLRU()
 	{
 		temp->LRU = lru;
 		lru++;
+		if (temp->next == 0)
+		{
+			last = temp;
+		}
 		temp = temp->next;
 	}
 }
